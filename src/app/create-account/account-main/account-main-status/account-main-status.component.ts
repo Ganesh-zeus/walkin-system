b2beaadf3aa60321d24ed97ffcdda34300ba1806
Ganesh-zeus <ganesh.moota@zeuslearning.com>
2021-08-19 14:47:49 +0530

    create account status bar works as expected

--- src/app/create-account/account-main/account-main-status/account-main-status.component.ts
+++ src/app/create-account/account-main/account-main-status/account-main-status.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {  ActivatedRoute, Router } from '@angular/router';
+import {  ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-account-main-status',
@@ -8,9 +8,35 @@ import {  ActivatedRoute, Router } from '@angular/router';
 })
 export class AccountMainStatusComponent implements OnInit {
 
+  currentUrl:string;
+  completionStatus:[boolean,boolean,boolean];
+
   constructor(private router:Router,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.currentUrl = this.router.url;
+    this.setCompletionStatus();
+    
+    this.router.events.subscribe((event) => {
+      if(event instanceof NavigationEnd){
+        this.currentUrl = event.url;
+        this.setCompletionStatus();
+      }
+    })
+  }
+
+  setCompletionStatus(){
+    if(this.currentUrl === '/create-account/personal'){
+      this.completionStatus = [true,false,false];
+    }
+
+    if(this.currentUrl === '/create-account/qualification'){
+      this.completionStatus = [true,true,false];
+    }
+
+    if(this.currentUrl === '/create-account/review'){
+      this.completionStatus = [true,true,true];
+    }
   }
 
   goTo(step:string){
